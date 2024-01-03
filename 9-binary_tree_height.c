e956c367631ed65d30f7c6a5191668f8de3c8ffc
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-03 11:07:52 +0100

    fixed the height added the null check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t* tree)
 	size_t left = 0;
 
 	if (tree == NULL)
-		return (0)
+		return (0);
 	if (tree->left != NULL)
 	{
 		left += binary_tree_height(tree->left) + 1;

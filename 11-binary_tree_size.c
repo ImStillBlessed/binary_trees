c55f68a1ec69d49551517f9808c8d8cda728b613
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-03 11:27:27 +0100

    check size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,7 +9,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 1;
 
-	if (tree == NULL);
+	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL)
 		size += binary_tree_size(tree->left);
